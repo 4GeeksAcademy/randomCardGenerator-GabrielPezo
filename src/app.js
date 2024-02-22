9fe77d569c97e9c60483e92d9b0ed7a73414f8ea
Gabriel Pezo Rojas <156217567+gabrielpezo@users.noreply.github.com>
2024-02-22 20:58:04 +0000

    randomcards

--- src/app.js
+++ src/app.js
@@ -5,7 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(randomPinta());
+  document.querySelector(".card").innerHTML = randomNumber();
 };
+
+function randomNumber() {
+  let numberArr = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomizer = Math.floor(Math.random() * 13);
+  return numberArr[randomizer];
+}
+function randomPinta() {
+  let pintaArr = ["diamond", "spade", "heart", "club"];
+  let randomizer2 = Math.floor(Math.random() * 4);
+  return pintaArr[randomizer2];
+}
